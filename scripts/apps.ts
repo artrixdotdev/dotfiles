1502d87ea33ceeafb3c254fe467d5a9cd0baaa58
Artrix9095 <sefu786@outlook.com>
2025-03-01 19:39:45 -0800

    Create a semi usable config

---
+++ scripts/apps.ts
@@ -0,0 +1,27 @@
+#!/usr/bin/env bun
+
+import { $ } from "bun";
+const args = Bun.argv.slice(2);
+const HOME = Bun.env.HOME;
+
+const settings: Record<string, string> = await Bun.file(
+  `${HOME}/.settings.json`,
+).json();
+
+if (args.length < 1) {
+  console.log("Usage: apps.ts <app>");
+  process.exit(1);
+}
+
+console.log(settings);
+
+const app = args[0];
+const rest = args.slice(1);
+
+if (settings && settings.apps && Object.keys(settings.apps).includes(app)) {
+  const executable = settings.apps[app];
+
+  await $`${executable} ${rest.join(" ")}`;
+} else {
+  console.error(`Unknown app: ${app}`);
+}
