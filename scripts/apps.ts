12e04757c1d8340040236f52ab00f6a79817f8a2
Artrix9095 <sefu786@outlook.com>
2025-03-02 07:03:03 -0800

    Homekeeping on apps script

--- scripts/apps.ts
+++ scripts/apps.ts
@@ -1,28 +1,25 @@
 #!/usr/bin/env bun
 
-import { $ } from "bun";
 const args = Bun.argv.slice(2);
 const HOME = Bun.env.HOME;
 
-const settings: Record<string, string> = await Bun.file(
-  `${HOME}/.settings.json`,
+const settings: { apps: Record<string, string> } = await Bun.file(
+   `${HOME}/.settings.json`,
 ).json();
 
 if (args.length < 1) {
-  console.log("Usage: apps.ts <app>");
-  process.exit(1);
+   console.log("Usage: apps.ts <app>");
+   process.exit(1);
 }
-
-console.log(settings);
 
 const app = args[0];
 const rest = args.slice(1);
 
-if (settings && settings.apps && Object.keys(settings.apps).includes(app)) {
-  const executable = settings.apps[app];
+if (settings?.apps && Object.keys(settings.apps).includes(app)) {
+   const executable = settings.apps[app];
+   const proc = Bun.spawn([executable, ...rest]);
 
-  const proc = Bun.spawn([executable, ...rest]);
-  proc.unref(); // Save memory
+   proc.unref(); // Save memory
 } else {
-  console.error(`Unknown app: ${app}`);
+   console.error(`Unknown app: ${app}`);
 }
