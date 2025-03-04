ce220644309096aa3ea577621615f7345c9bd9e4
Artrix9095 <sefu786@outlook.com>
2025-03-04 09:19:39 -0800

    Add better delays to refresh script

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+# Add a delay before refreshing if it causes issues
+sleep_time=0 # Change to 0.5 if needed
 
 ags quit
-sleep 0.3
+
+sleep "$sleep_time"
+
 ags run --gtk4
 
-sleep 1
+sleep "$((sleep_time + 1))"
 
 notify-send "Refreshed!"
+
