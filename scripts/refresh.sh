e5afdfcc92aadd4f16e2f6149c546c781203fe0f
Artrix9095 <sefu786@outlook.com>
2025-03-16 19:03:03 -0700

    Update hyprland.conf

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -3,6 +3,9 @@
 # Add a delay before refreshing if it causes issues
 sleep_time=0 # Change to 0.5 if needed
 
+
+hyprctl reload
+
 ags quit
 
 sleep "$sleep_time"
@@ -12,4 +15,3 @@ ags run --gtk4
 sleep "$((sleep_time + 1))"
 
 notify-send "Refreshed!"
-
