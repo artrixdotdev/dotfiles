c22096b328fc7d4c4de2c0513588773aadc67f26
Artrix9095 <sefu786@outlook.com>
2025-07-21 13:28:53 -0700

    Add a rustfmt

--- core/src/main.rs
+++ core/src/main.rs
@@ -10,28 +10,28 @@ use commands::{daemon::DaemonCommand, render::RenderCommand};
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// Root directory for all relative paths. Defaults to $HOME/dotfiles.
-    #[arg(short, long, global = true)]
-    root_dir: Option<PathBuf>,
+   /// Root directory for all relative paths. Defaults to $HOME/dotfiles.
+   #[arg(short, long, global = true)]
+   root_dir: Option<PathBuf>,
 
-    #[command(subcommand)]
-    command: Commands,
+   #[command(subcommand)]
+   command: Commands,
 }
 
 #[derive(Subcommand, Debug)]
 enum Commands {
-    /// Render Handlebars templates once
-    Render(RenderCommand),
-    /// Watch for changes and automatically re-render
-    Daemon(DaemonCommand),
+   /// Render Handlebars templates once
+   Render(RenderCommand),
+   /// Watch for changes and automatically re-render
+   Daemon(DaemonCommand),
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
+   let args = Args::parse();
 
-    match args.command {
-        Commands::Render(cmd) => cmd.execute(args.root_dir).await,
-        Commands::Daemon(cmd) => cmd.execute(args.root_dir).await,
-    }
+   match args.command {
+      Commands::Render(cmd) => cmd.execute(args.root_dir).await,
+      Commands::Daemon(cmd) => cmd.execute(args.root_dir).await,
+   }
 }
