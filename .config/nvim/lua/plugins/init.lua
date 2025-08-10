12b5c11cba12d3ce35ba8b339ef5e5cc7b3b92c7
artrixdotdev <sefu786@outlook.com>
2025-08-09 23:06:12 -0700

    Update nvim deps

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -268,7 +268,6 @@ return {
    },
    {
       "mrcjkb/rustaceanvim",
-      version = "^5", -- Recommended
       lazy = false, -- This plugin is already lazy
       ft = "rust",
       config = function()
