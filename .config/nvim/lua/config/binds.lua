3dff67963a1cbfec255c2cb171a08dd421fcf8f9
artrixdotdev <sefu786@outlook.com>
2025-08-31 00:07:41 -0700

    Add more binds

--- .config/nvim/lua/config/binds.lua
+++ .config/nvim/lua/config/binds.lua
@@ -6,7 +6,7 @@ wk.add {
 
    -- Misc
    { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>", mode = "n", desc = "Toggle undo tree" },
-
+   { "<leader>s", "<cmd>Telescope lsp_document_symbols<cr>", mode = "n", desc = "Show LSP symbols" },
    -- Clipboard
    { "cp", '"+y"', mode = { "n", "v" }, desc = "Copy to clipboard", group = "clipboard" },
    { "cv", '"+p"', mode = { "n", "v" }, desc = "Paste from clipboard", group = "clipboard" },
@@ -14,5 +14,6 @@ wk.add {
    -- File
    { "<leader>f", group = "file" },
    { "<leader>ft", "<cmd>Neotree position=right toggle <cr>", desc = "Open file tree", mode = "n" },
+   { "<leader>fw", "<cmd>Telescope live_grep<cr>", desc = "Search word", mode = "n" },
    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find File", mode = "n" },
 }
