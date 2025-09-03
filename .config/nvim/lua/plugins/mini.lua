09b7545d778169010a9efdcff669db5ee8340689
artrixdotdev <sefu786@outlook.com>
2025-09-02 22:17:59 -0700

    Move to mini.nvim for utility

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,7 @@
+return {
+   { "nvim-mini/mini.ai", version = false, opts = {} },
+   { "nvim-mini/mini.surround", version = false, opts = {} },
+   { "nvim-mini/mini.operators", version = false, opts = {} },
+   { "nvim-mini/mini.pairs", version = false, opts = {} },
+   { "nvim-mini/mini.bracketed", version = false, opts = {} },
+}
