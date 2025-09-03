a6409679c10633673439b0ba9b656f3a880fcf8b
artrixdotdev <sefu786@outlook.com>
2025-09-02 22:16:53 -0700

    Make theme work

--- .config/nvim/lua/config/theme.lua
+++ .config/nvim/lua/config/theme.lua
@@ -1,25 +1,25 @@
-local M = {}
-
-M.base46 = {
-   theme = "blossom_light",
-   changed_themes = {
-      catppuccin = {
-         base_30 = {
-            black = "#12121c",
-            darker_black = "#0e0e17",
-         },
-         base_16 = {
-            base00 = "#101019",
-            base01 = "#12121c",
-            base04 = "#0e0e17",
-         },
-      },
-   },
-   -- hl_override = {
-   -- 	Comment = { italic = true },
-   -- 	["@comment"] = { italic = true },
-   -- },
-}
+-- local M = {}
+--
+-- M.base46 = {
+--    theme = "blossom_light",
+--    changed_themes = {
+--       catppuccin = {
+--          base_30 = {
+--             black = "#12121c",
+--             darker_black = "#0e0e17",
+--          },
+--          base_16 = {
+--             base00 = "#101019",
+--             base01 = "#12121c",
+--             base04 = "#0e0e17",
+--          },
+--       },
+--    },
+--    -- hl_override = {
+--    -- 	Comment = { italic = true },
+--    -- 	["@comment"] = { italic = true },
+--    -- },
+-- }
 
 -- local theme = require("base46-extracted").get_theme_tb "base_30"
 --
@@ -27,4 +27,6 @@ M.base46 = {
 --    print(k .. " = " .. v)
 -- end
 
-return M
+return {
+   theme = "catppuccin",
+}
