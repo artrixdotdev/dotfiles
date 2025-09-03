bfe4cf86036140669d460a1b09e569b700985b4b
artrixdotdev <sefu786@outlook.com>
2025-09-02 22:17:52 -0700

    Remove dependancy on base46 for telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
    "nvim-telescope/telescope.nvim",
    event = "VeryLazy",
-   requires = { { "nvim-lua/plenary.nvim", "nvchad/base46" } },
+   requires = { { "nvim-lua/plenary.nvim", "artrixdotdev/base46-extracted" } },
    opts = {},
 }
