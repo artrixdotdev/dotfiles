8d52330e79a001fc153cd552d5b4985232cd1c74
artrixdotdev <sefu786@outlook.com>
2025-09-04 16:12:23 -0700

    Add kdl formatter

--- .config/nvim/lua/config/formatters.lua
+++ .config/nvim/lua/config/formatters.lua
@@ -1,6 +1,7 @@
 -- All formatters must be installed via Mason or in the system path.
 return {
-    lua = { "stylua" },
-    rust = { "rustfmt", lsp_format = "fallback" },
-    javascript = { "prettierd", "prettier", stop_after_first = true },
+   lua = { "stylua" },
+   rust = { "rustfmt", lsp_format = "fallback" },
+   javascript = { "prettierd", "prettier", stop_after_first = true },
+   kdl = { "kdlfmt" },
 }
