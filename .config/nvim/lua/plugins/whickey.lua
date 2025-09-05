59b5aafa21c60dcd45e7ac085855e85f071b4652
artrixdotdev <sefu786@outlook.com>
2025-09-04 19:53:11 -0700

    Remove useless opts function for whichkey

--- .config/nvim/lua/plugins/whickey.lua
+++ .config/nvim/lua/plugins/whickey.lua
@@ -1,15 +1,11 @@
-dofile(vim.g.base46_cache .. "whichkey")
-
 -- Visually displays keybindings and their descriptions
 return {
    "folke/which-key.nvim",
    event = "VeryLazy",
    keys = { "<leader>", "<c-w>", '"', "'", "`", "c", "v", "g" },
 
-   opts = function()
-      return {
-         preset = "helix",
-         delay = 500,
-      }
-   end,
+   opts = {
+      preset = "helix",
+      delay = 500,
+   },
 }
