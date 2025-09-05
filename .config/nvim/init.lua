dd60146fa4af5e1d92bc28c8e83139a9e82a087d
artrixdotdev <sefu786@outlook.com>
2025-09-04 20:59:08 -0700

    Add more highlight defs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -118,3 +118,27 @@ require("base46-extracted").install_integration("defaults", {
    LazyReasonImport = { fg = theme.white },
    LazyProgressDone = { fg = theme.green },
 })
+
+require("base46-extracted").install_integration(
+   "tokens",
+   {
+      ["@lsp.type.class"] = { link = "Structure" },
+      ["@lsp.type.decorator"] = { link = "Function" },
+      ["@lsp.type.enum"] = { link = "Type" },
+      ["@lsp.type.enumMember"] = { link = "Constant" },
+      ["@lsp.type.function"] = { link = "@function" },
+      ["@lsp.type.interface"] = { link = "Structure" },
+      ["@lsp.type.macro"] = { link = "@macro" },
+      ["@lsp.type.method"] = { link = "@function.method" },
+      ["@lsp.type.namespace"] = { link = "@module" },
+      ["@lsp.type.parameter"] = { link = "@variable.parameter" },
+      ["@lsp.type.property"] = { link = "@property" },
+      ["@lsp.type.struct"] = { link = "Structure" },
+      ["@lsp.type.type"] = { link = "@type" },
+      ["@lsp.type.typeParamater"] = { link = "TypeDef" },
+      ["@lsp.type.variable"] = { link = "@variable" },
+      ["@event"] = { fg = theme.base08 },
+      ["@modifier"] = { fg = theme.base08 },
+      ["@regexp"] = { fg = theme.base0F },
+   }
+)
