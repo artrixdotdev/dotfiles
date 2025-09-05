f2fd625096ea360ff5864bd884c6ac5824d45f50
artrixdotdev <sefu786@outlook.com>
2025-09-04 19:55:14 -0700

    Use thinner pipe for blankline indent indicator

--- .config/nvim/lua/plugins/blankline.lua
+++ .config/nvim/lua/plugins/blankline.lua
@@ -1,7 +1,10 @@
 return {
    "lukas-reineke/indent-blankline.nvim",
+   dependencies = { "nvim-treesitter/nvim-treesitter" },
    main = "ibl",
    ---@module "ibl"
    ---@type ibl.config
-   opts = {},
+   opts = {
+      indent = { char = "│" },
+   },
 }
