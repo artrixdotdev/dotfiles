4b4a4c0c1a8f66180403f1c9216a645b3a5e5d38
artrixdotdev <sefu786@outlook.com>
2025-09-04 20:07:15 -0700

    Move mason to base46-extracted

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,8 +1,22 @@
-dofile(vim.g.base46_cache .. "mason")
-
 -- LSP, DAP, and linter manager
 return {
    "mason-org/mason.nvim",
+   dependencies = {
+      "artrixdotdev/base46-extracted",
+   },
+   on_init = function()
+      local colors = require("base46-extracted").get_theme_tb "base_30"
+
+      require("base46-extracted").install_integration("mason", {
+         MasonHeader = { bg = colors.red, fg = colors.black },
+         MasonHighlight = { fg = colors.blue },
+         MasonHighlightBlock = { fg = colors.black, bg = colors.green },
+         MasonHighlightBlockBold = { link = "MasonHighlightBlock" },
+         MasonHeaderSecondary = { link = "MasonHighlightBlock" },
+         MasonMuted = { fg = colors.light_grey },
+         MasonMutedBlock = { fg = colors.light_grey, bg = colors.one_bg },
+      })
+   end,
    opts = {
       ui = {
          icons = {
