ac8c877b5cb8faff59efe481089fdb966c4d8de7
artrixdotdev <sefu786@outlook.com>
2025-10-02 10:42:41 -0700

    Add typescript & tailwind tools

--- .config/nvim/lua/plugins/javascript.lua
+++ .config/nvim/lua/plugins/javascript.lua
@@ -1,3 +1,23 @@
 return {
-   { "davidmh/mdx.nvim", config = true, dependencies = { "nvim-treesitter/nvim-treesitter" } },
+   {
+      "davidmh/mdx.nvim",
+      config = true,
+      dependencies = { "nvim-treesitter/nvim-treesitter" },
+   },
+   {
+      "luckasRanarison/tailwind-tools.nvim",
+      name = "tailwind-tools",
+      build = ":UpdateRemotePlugins",
+      dependencies = {
+         "nvim-treesitter/nvim-treesitter",
+         "nvim-telescope/telescope.nvim",
+         "neovim/nvim-lspconfig",
+      },
+      opts = {}, -- your configuration
+   },
+   {
+      "pmizio/typescript-tools.nvim",
+      dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+      opts = {},
+   },
 }
