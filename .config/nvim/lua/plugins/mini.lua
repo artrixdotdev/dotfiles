a26c717cb532810270a4e980f4cda2a6f85ff2da
artrixdotdev <sefu786@outlook.com>
2026-06-01 13:46:00 +0200

    Configure mini ai textobjects

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,5 +1,22 @@
 return {
-   { "nvim-mini/mini.ai", version = false, opts = {} },
+   {
+      "nvim-mini/mini.ai",
+      version = false,
+      opts = {
+         mappings = {
+            around = "a",
+            inside = "i",
+            around_next = "an",
+            inside_next = "in",
+            around_last = "al",
+            inside_last = "il",
+            goto_left = "g[",
+            goto_right = "g]",
+         },
+         n_lines = 500,
+         search_method = "cover_or_next",
+      },
+   },
    { "nvim-mini/mini.surround", version = false, opts = {} },
    { "nvim-mini/mini.operators", version = false, opts = {} },
    { "nvim-mini/mini.pairs", version = false, opts = {} },
