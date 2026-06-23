d98eb17b9058b8a70b99a6eb99616ec7b956c27e
artrixdotdev <sefu786@outlook.com>
2026-06-23 13:05:00 -0700

    Select JavaScript formatter by project

--- .config/nvim/lua/config/formatters.lua
+++ .config/nvim/lua/config/formatters.lua
@@ -1,7 +1,17 @@
 -- All formatters must be installed via Mason or in the system path.
+local function get_js_formatter(bufnr)
+   if vim.fs.root(bufnr, { "biome.json", "biome.jsonc" }) then
+      return { "biome" }
+   end
+   return { "prettierd", "prettier", stop_after_first = true }
+end
+
 return {
    lua = { "stylua" },
    rust = { "rustfmt", lsp_format = "fallback" },
-   javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+   javascript = get_js_formatter,
+   javascriptreact = get_js_formatter,
+   typescript = get_js_formatter,
+   typescriptreact = get_js_formatter,
    kdl = { "kdlfmt" },
 }
